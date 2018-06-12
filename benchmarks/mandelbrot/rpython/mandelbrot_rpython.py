ac9476035c8662898dfab1c140331304d5c3a2d0
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-12 09:13:33 +0200

    Mandelbrot rendering - RPython implementation

---
+++ benchmarks/mandelbrot/rpython/mandelbrot_rpython.py
@@ -0,0 +1,50 @@
+# vim: set fileencoding=utf-8
+
+import palette_mandmap
+from sys import argv, exit
+
+
+def calc_mandelbrot(width, height, maxiter, palette):
+    print "P3"
+    print width, height
+    print "255"
+
+    cy = -1.5
+    for y in range(0, height):
+        cx = -2.0
+        for x in range(0, width):
+            zx = 0.0
+            zy = 0.0
+            i = 0
+            while i < maxiter:
+                zx2 = zx * zx
+                zy2 = zy * zy
+                if zx2 + zy2 > 4.0:
+                    break
+                zy = 2.0 * zx * zy + cy
+                zx = zx2 - zy2 + cx
+                i += 1
+
+            r = palette[i][0]
+            g = palette[i][1]
+            b = palette[i][2]
+            print r, g, b
+            cx += 3.0/width
+        cy += 3.0/height
+
+
+def entry_point(argv):
+    if len(argv) < 4:
+        print("usage: ./mandelbrot width height maxiter")
+        return 1
+
+    width = int(argv[1])
+    height = int(argv[2])
+    maxiter = int(argv[3])
+    calc_mandelbrot(width, height, maxiter, palette_mandmap.palette)
+    return 0
+
+
+def target(driver, args):
+    print "*** target ***"
+    return entry_point, None
